2dd3016103e332e89fb581ca98e5084e463ef544
Karl <git@karlneugebauer.com>
2020-07-26 17:19:18 +0200

    more api interface memes

--- src/api/IKraken.ts
+++ src/api/IKraken.ts
@@ -1,5 +1,29 @@
 "use strict"
 
+export interface IKrakenError {
+  message: string,
+  status: number,
+  error: string
+}
+
+/** kraken/users/XXXX */
+export interface IKrakenUser {
+  display_name: string,
+  _id: string,
+  name: string,
+  type: string,
+  bio: string,
+  created_at: string,
+  uddated_at: string,
+  logo: string
+}
+
+/** kraken/users?id= */
+export interface IKrakenUsers {
+  _total: number,
+  users: IKrakenUser[]
+}
+
 /** kraken/users/XXXX/chat */
 export interface IKrakenUsersChat {
   id: string,
@@ -49,6 +73,95 @@ export interface IKrakenChannel {
 export interface IKrakenChannels {
   _total: number,
   channels: IKrakenChannel[]
+}
+
+export interface IKrakenStream {
+  _id: number,
+  game: string,
+  broadcast_platform: string,
+  community_id: string,
+  community_ids: string[], //TODO:???
+  viewers: number,
+  video_height: number,
+  average_fps: number,
+  delay: number,
+  created_at: string,
+  is_playlist: boolean,
+  stream_type: string,
+  preview: {
+    small: string,
+    medium: string,
+    large: string,
+    template: string
+  },
+  channel: IKrakenChannel
+}
+
+export interface IKrakenStreams {
+  stream: null | IKrakenStream
+}
+
+export interface IKrakenChannelVideo {
+  title: string,
+  description: string | null,
+  description_html: string | null,
+  broadcast_id: number,
+  broadcast_type: string,
+  status: string,
+  tag_list: string,
+  views: number,
+  url: string,
+  language: string,
+  created_at: string,
+  viewable: string,
+  viewable_at: null | string,
+  published_at: string,
+  delete_at: string,
+  _id: string,
+  recorded_at: string,
+  game: string,
+  length: number,
+  preview: {
+    small: string,
+    medium: string,
+    large: string,
+    template: string
+  },
+  animated_preview_url: string,
+  thumbnails: {
+    small: {
+      type: string,
+      url: string
+    }[],
+    medium:
+      {
+        type: string,
+        url: string
+      }[],
+    large: {
+      type: string,
+      url: string
+    }[],
+    template: {
+      type: string,
+      url: string
+    }[]
+  },
+  fps: {
+    chunked: number
+  },
+  seek_previews_url: string,
+  resolutions: {
+    chunked: string
+  },
+  restriction: string,
+  channel: IKrakenChannel,
+  increment_view_count_url: string
+}
+
+export interface IKrakenChannelVideos {
+  _total: number,
+  videos: IKrakenChannelVideo[]
 }
 
 export interface IKrakenFollowsChannel {
--- src/api/Kraken.ts
+++ src/api/Kraken.ts
@@ -6,10 +6,17 @@ import {Logger} from "../helper/Logger";
 import {TimeConversion} from "../Enums";
 
 import {
+  IKrakenError,
+  IKrakenUser,
+  IKrakenUsers,
   IKrakenUsersChat,
   IKrakenUsersChatChannel,
   IKrakenChannel,
   IKrakenChannels,
+  IKrakenStream,
+  IKrakenStreams,
+  IKrakenChannelVideo,
+  IKrakenChannelVideos,
   IKrakenFollowsChannel
 } from "./IKraken"
 
@@ -30,7 +37,7 @@ export class Kraken {
     return this._bot
   }
 
-  async request (pathAppend: string, method: Method = 'GET'): Promise<any> {
+  async request<T> (pathAppend: string, method: Method = 'GET'): Promise<IKrakenError | T> {
     try {
       let result = await Axios({
         url: `https://api.twitch.tv/kraken/${pathAppend}`,
@@ -44,29 +51,29 @@ export class Kraken {
       return result.data
     } catch (e) {
       Logger.info(e)
+      return e.result.data
     }
-    return {}
   }
 
   /**
    * Gets info about current live broadcast for channelID
    */
-  async streamInfo (channelID: number | string): Promise<any> {
-    return await this.request(`streams/${channelID}`)
+  async streamInfo (channelID: number | string): Promise<IKrakenError | IKrakenStreams> {
+    return await this.request<IKrakenStreams>(`streams/${channelID}`)
   }
 
   /**
    * Get an array with info of past 100 broadcast vods
    */
-  async getVods (channelID: number | string): Promise<any> {
-    return await this.request(`channels/${channelID}/videos?broadcast_type=archive&limit=100`)
+  async getVods (channelID: number | string): Promise<IKrakenError | IKrakenChannelVideos> {
+    return await this.request<IKrakenChannelVideos>(`channels/${channelID}/videos?broadcast_type=archive&limit=100`)
   }
 
 //TODO: cleanup of duplicated stuff
   /**
    * receive login name from a single userid
    */
-  async loginFromUserId (userId: number | string): Promise<any> {
+  async loginFromUserId (userId: number | string): Promise<string> {
     let response = await this.userInfo(userId)
     if (Object.hasOwnProperty.call(response, 'login')) {
       return response.login
@@ -80,9 +87,9 @@ export class Kraken {
    * Returns the userId from a single login
    */
   async userIdFromLogin (username: string): Promise<number | string> {
-    let response: { total: number, users: { _id: number | string }[] } = await this.userInfosFromLogins([username])
+    let response = await this.userInfosFromLogins([username])
 
-    if (response.total === 0 || response.users.length === 0) {
+    if (response._total === 0 || response.users.length === 0) {
       return '-1'
     } else {
       return response.users[0]["_id"]
@@ -95,10 +102,17 @@ export class Kraken {
    * directly returns the ["users"]
    * automatically handles if more than 100 usernames are requested
    */
-  async userDataFromIds (ids: string[]): Promise<object[]> {
+  async userDataFromIds (userIds: number[] | string[]): Promise<IKrakenUser[]> {
     let chunkSize = 100
-    let users: object[] = []
-    let requestChunks = [].concat.apply([], ids.map((elem: string, i: number) => i % chunkSize ? [] : [ids.slice(i, i + chunkSize)]))
+    let users: IKrakenUser[] = []
+
+    let requestChunks: (number[] | string[])[] = []
+
+    for (let i = 0; i < userIds.length; i += chunkSize) {
+      requestChunks.push(userIds.slice(i, i + chunkSize))
+    }
+
+    //let requestChunks = [].concat.apply([], userIds.map((elem: string, i: number) => i % chunkSize ? [] : [userIds.slice(i, i + chunkSize)]))
 
     for (let chunk of requestChunks) {
       let responseChunk = await this.userInfosFromIds(chunk)
@@ -115,15 +129,22 @@ export class Kraken {
    * directly returns the ["users"]
    * automatically handles if more than 100 usernames are requested
    */
-  async userDataFromLogins (usernames: string[]): Promise<object[]> {
+  async userDataFromLogins (usernames: string[]): Promise<IKrakenUser[]> {
     let chunkSize = 100
-    let users: object[] = []
-    let requestChunks = [].concat.apply([], usernames.map((elem, i) => i % chunkSize ? [] : [usernames.slice(i, i + chunkSize)]))
+    let users: IKrakenUser[] = []
+
+    let requestChunks: string[][] = []
+
+    for (let i = 0; i < usernames.length; i += chunkSize) {
+      requestChunks.push(usernames.slice(i, i + chunkSize))
+    }
+
+    //let requestChunks = [].concat.apply([], usernames.map((elem, i) => i % chunkSize ? [] : [usernames.slice(i, i + chunkSize)]))
 
     for (let chunk of requestChunks) {
       let responseChunk = await this.userInfosFromLogins(chunk)
-      if (responseChunk["_total"] > 0) {
-        users = users.concat(responseChunk["users"])
+      if (responseChunk._total > 0) {
+        users = users.concat(responseChunk.users)
       }
     }
     return users
@@ -133,7 +154,7 @@ export class Kraken {
    * Return the userInfo from an array of ids
    * max 100 entries are allowed
    */
-  async userInfosFromIds (ids: string[]): Promise<object> {
+  async userInfosFromIds (ids: number[] | string[]): Promise<IKrakenError | IKrakenUsers> {
     return await this.request('users?id=' + ids.join(','))
   }
 
@@ -141,67 +162,36 @@ export class Kraken {
    * Return the userInfo from an array of usernames
    * max 100 entries are allowed
    */
-  async userInfosFromLogins (usernames: string[]): Promise<object> {
-    usernames.map((entry) => {
-      return entry.replace(/#/, '')
-    })
+  async userInfosFromLogins (usernames: string[]): Promise<IKrakenError | IKrakenUsers> {
+    usernames.map((entry: string) => entry.replace(/#/, ''))
     return await this.request('users?login=' + usernames.join(','))
   }
 
-
   /**
-   * Users object returned by
-   * kraken/users/XXXXXX/chat and
-   * kraken/users/XXXXXX/chat/channels/YYYYYY
-   *
-   * @typedef {Object} Users
-   * @property {string} id
-   * @property {string} login
-   * @property {string} displayName
-   * @property {string} color
-   * @property {boolean} isVerifiedBot
-   * @property {boolean} isKnownBot
-   * @property {Object} Badges
+   * Accesses kraken/users/userID/chat
    */
-
-  /**
-   * Accesses the kraken/users/:userID/chat
-   * Example: https://api.twitch.tv/kraken/users/38949074/chat?api_version=5
-   * Example return:
-   * {
-   *   "id":"38949074",
-   *   "login":"icdb",
-   *   "displayName":"icdb",
-   *   "color":"#00FF00",
-   *   "isVerifiedBot":false,
-   *   "isKnownBot":false,
-   *   "badges":[]
-   * }
-   * @param  {String|int} userId The userID to check for
-   * @return {Users} [description]
-   */
-  async userInfo (userId) {
+  async userInfo (userId: number | string): Promise<IKrakenError | IKrakenUsersChat> {
     return await this.request('users/' + userId + '/chat')
   }
 
   /**
    * Accesses kraken/users/userID/chat/channels/roomID
    */
-  async userInChannelInfo (userId: number | string, roomId: number | string): Promise<IKrakenUsersChatChannel> {
+  async userInChannelInfo (userId: number | string, roomId: number | string): Promise<IKrakenError | IKrakenUsersChatChannel> {
     return await this.request('users/' + userId + '/chat/channels/' + roomId)
   }
 
   /**
    * Accesses kraken/channels/roomId
    */
-  async channelInfo (roomId: number | string): Promise<IKrakenChannel> {
+  async channelInfo (roomId: number | string): Promise<IKrakenError | IKrakenChannel> {
     return await this.request('channels/' + roomId)
   }
 
   /**
    * Get Channel objects for an array of roomIds
    */
-  async channelInfos (roomIds: number[] | string[]): Promise<IKrakenChannels> {
+  async channelInfos (roomIds: number[] | string[]): Promise<IKrakenError | IKrakenChannels> {
     return await this.request('channels?id=' + roomIds.join(','))
   }
 
@@ -210,16 +200,23 @@ export class Kraken {
    */
   async channelInfosFromIds (roomIds: number[] | string[]): Promise<IKrakenChannel[]> {
     let chunkSize = 100
-    let users = []
-    let requestChunks = [].concat.apply([], roomIds.map((elem, i) => i % chunkSize ? [] : [roomIds.slice(i, i + chunkSize)]))
+    let channels: IKrakenChannel[] = []
+
+    let requestChunks: (number[] | string[])[] = []
+
+    for (let i = 0; i < roomIds.length; i += chunkSize) {
+      requestChunks.push(roomIds.slice(i, i + chunkSize))
+    }
+
+    //let requestChunks = [].concat.apply([], roomIds.map((elem, i) => i % chunkSize ? [] : [roomIds.slice(i, i + chunkSize)]))
 
     for (let chunk of requestChunks) {
       let responseChunk = await this.channelInfos(chunk)
       if (responseChunk["_total"] > 0) {
-        users = users.concat(responseChunk["channels"])
+        channels = channels.concat(responseChunk["channels"])
       }
     }
-    return users
+    return channels
   }
 
   async followTime (userId: number | string, roomId: number | string): Promise<{ followDate: Date | undefined, followTimeMs: number, followTimeS: number, followtimeMin: number, followtimeH: number, followtimeD: number, followtimeMon: number, followtimeY: number }> {
