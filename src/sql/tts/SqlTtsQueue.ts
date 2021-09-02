616c180ce454ccb7d7a771ee7deec66592cf57f1
Karl <git@karlneugebauer.com>
2021-09-02 13:44:16 +0200

    DB adjustment

--- src/sql/tts/SqlTtsQueue.ts
+++ src/sql/tts/SqlTtsQueue.ts
@@ -15,7 +15,7 @@ export class SqlTtsQueue {
                                                 RequesterDisplayName,
                                                 IsSubOrHigher,
                                                 RawMessage,
-                                                MessageId,
+                                                RedemptionId,
                                                 WasTimedOut,
                                                 RequestTimestamp)
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?);`,
